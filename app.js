ff5c6e36b908b530a53e27732e04306c6ac1dcd6
Hunter <whwebster98@live.com>
2023-01-23 00:00:12 -0600

    module 3 work

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const  hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
-var hbs = require('hbs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
-travel
+    travel
 };
